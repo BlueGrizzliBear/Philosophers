5b4848bbdc0f9ed4a7e1437a6dd3f5abeb1b36aa
Clement Bussiere <you@example.com>
2020-10-07 17:36:32 +0200

    try

--- philo_one/srcs/alive.c
+++ philo_one/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/10/07 17:34:29 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/10/07 17:36:21 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,20 +37,20 @@ int		ft_lock_forks(t_phi *phi)
 	int ret;
 
 	ret = 0;
-	// while (phi->left_fork->id == phi->right_fork->id ||
-	// !(phi->right_fork->status == 0 && phi->left_fork->status == 0))
-	// {
-	// 	if (ft_is_dead(phi) == 1 || phi->params->game == 0)
-	// 		return (1);
-	// }
+	while (phi->left_fork->id == phi->right_fork->id ||
+	!(phi->right_fork->status == 0 && phi->left_fork->status == 0))
+	{
+		if (ft_is_dead(phi) == 1 || phi->params->game == 0)
+			return (1);
+	}
 	// if (pthread_mutex_lock(phi->params->available))
 	// 	return (ft_error(ERROR_LOCK_MUTEX));
-	// phi->right_fork->status = 1;
-	// phi->left_fork->status = 1;
 	if (pthread_mutex_lock(phi->left_fork->mutex))
 		return (ft_error(ERROR_LOCK_MUTEX));
+	phi->left_fork->status = 1;
 	if (pthread_mutex_lock(phi->right_fork->mutex))
 		return (ft_error(ERROR_LOCK_MUTEX));
+	phi->right_fork->status = 1;
 	// if (pthread_mutex_unlock(phi->params->available))
 	// 	return (ft_error(ERROR_UNLOCK_MUTEX));
 	if ((ret = ft_display(phi, "has taken a fork\n")) ||
@@ -61,11 +61,12 @@ int		ft_lock_forks(t_phi *phi)
 
 int		ft_unlock_forks(t_phi *phi)
 {
-	if (pthread_mutex_unlock(phi->right_fork->mutex) ||
-	pthread_mutex_unlock(phi->left_fork->mutex))
+	if (pthread_mutex_unlock(phi->right_fork->mutex))
 		return (ft_error(ERROR_UNLOCK_MUTEX));
-	// phi->right_fork->status = 0;
-	// phi->left_fork->status = 0;
+	phi->right_fork->status = 0;
+	if (pthread_mutex_unlock(phi->left_fork->mutex))
+		return (ft_error(ERROR_UNLOCK_MUTEX));
+	phi->left_fork->status = 0;
 	return (0);
 }
 
