e14b69c16b0bbc277aaf3a673d6384db6094911d
Clement Bussiere <you@example.com>
2020-10-07 10:36:23 +0200

    modif

--- philo_one/srcs/alive.c
+++ philo_one/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/10/07 10:28:32 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/10/07 10:35:58 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,10 +69,10 @@ int		ft_lock_forks(t_phi *phi)
 
 int		ft_unlock_forks(t_phi *phi)
 {
-	phi->right_fork->status = 0;
 	phi->left_fork->status = 0;
-	if (pthread_mutex_unlock(phi->right_fork->mutex) ||
-	pthread_mutex_unlock(phi->left_fork->mutex))
+	phi->right_fork->status = 0;
+	if (pthread_mutex_unlock(phi->left_fork->mutex) ||
+	pthread_mutex_unlock(phi->right_fork->mutex))
 		return (ft_error(ERROR_UNLOCK_MUTEX));
 	return (0);
 }
