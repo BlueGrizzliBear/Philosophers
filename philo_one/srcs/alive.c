08ba538858b5123c0804439d0c366de24efe97fb
Clement Bussiere <you@example.com>
2020-10-07 10:50:22 +0200

    try

--- philo_one/srcs/alive.c
+++ philo_one/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/10/07 10:47:19 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/10/07 10:50:16 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,13 @@ int		ft_lock_forks(t_phi *phi)
 	if (pthread_mutex_lock(phi->params->available))
 		return (ft_error(ERROR_LOCK_MUTEX));
 	// here
-	while (phi->left_fork->id == phi->right_fork->id ||
-		(phi->left_fork->status == 1 || phi->right_fork->status == 1))
-	{
-		if (ft_is_dead(phi) == 1 || phi->params->game == 0)
-			return (-1);
-		usleep(10);
-	}
+	// while (phi->left_fork->id == phi->right_fork->id ||
+	// 	(phi->left_fork->status == 1 || phi->right_fork->status == 1))
+	// {
+	// 	if (ft_is_dead(phi) == 1 || phi->params->game == 0)
+	// 		return (-1);
+	// 	usleep(10);
+	// }
 	ret = 0;
 	if (pthread_mutex_lock(phi->left_fork->mutex))
 		return (ft_error(ERROR_LOCK_MUTEX));
