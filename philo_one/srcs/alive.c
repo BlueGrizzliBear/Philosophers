a00b5f73fed8bd18f2d5c1a6c45bda2d14ae71d0
Clement Bussiere <you@example.com>
2020-10-07 13:44:50 +0200

    try

--- philo_one/srcs/alive.c
+++ philo_one/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/10/07 13:35:32 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/10/07 13:44:40 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,12 +68,12 @@ int		ft_lock_forks(t_phi *phi)
 	if (pthread_mutex_lock(phi->left_fork->mutex))
 		return (ft_error(ERROR_LOCK_MUTEX));
 	phi->left_fork->status = 1;
-	if ((ret = ft_display(phi, "has taken a fork\n")))
+	if ((ret = fts_display(phi, "has taken a fork\n")))
 		return (ret < 0 ? -1 : ft_error(ERROR_DISPLAY));
 	if (pthread_mutex_lock(phi->right_fork->mutex))
 		return (ft_error(ERROR_LOCK_MUTEX));
 	phi->right_fork->status = 1;
-	if ((ret = ft_display(phi, "has taken a fork\n")))
+	if ((ret = fts_display(phi, "has taken a fork\n")))
 		return (ret < 0 ? -1 : ft_error(ERROR_DISPLAY));
 	// here
 	if (pthread_mutex_unlock(phi->params->available))
