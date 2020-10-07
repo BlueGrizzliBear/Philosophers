05ed5bf7b2e4c2994f479b40e6c03ea8bcdb3967
Clement Bussiere <you@example.com>
2020-10-07 13:17:40 +0200

    try

--- philo_one/srcs/alive.c
+++ philo_one/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/10/07 13:16:23 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/10/07 13:17:28 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,18 @@ int		ft_lock_forks(t_phi *phi)
 	// if (pthread_mutex_lock(phi->params->available))
 	// 	return (ft_error(ERROR_LOCK_MUTEX));
 	// here
-	while (phi->left_fork->id == phi->right_fork->id ||
-	phi->left_fork->status == 1 || phi->right_fork->status == 1 ||
-	pthread_mutex_lock(phi->params->available) != 0)
+	// while (phi->left_fork->id == phi->right_fork->id ||
+	// // phi->left_fork->status == 1 || phi->right_fork->status == 1 ||
+	// // pthread_mutex_lock(phi->params->available) != 0)
 	// pthread_mutex_lock(phi->params->available) != 0 ||
 	// phi->left_fork->status == 1 || phi->right_fork->status == 1)
-	{
-		if (pthread_mutex_unlock(phi->params->available))
-			return (ft_error(ERROR_UNLOCK_MUTEX));
-		if (ft_is_dead(phi) == 1 || phi->params->game == 0)
-			return (1);
-		usleep(10);
-	}
+	// {
+	// 	if (pthread_mutex_unlock(phi->params->available))
+	// 		return (ft_error(ERROR_UNLOCK_MUTEX));
+	// 	if (ft_is_dead(phi) == 1 || phi->params->game == 0)
+	// 		return (1);
+	// 	usleep(10);
+	// }
 
 	// here
 	// if (pthread_mutex_lock(phi->params->available))
