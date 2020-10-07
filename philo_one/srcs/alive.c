ea33a29d838f04db48fecb40a052d4d8bf9251a9
Clement Bussiere <you@example.com>
2020-10-07 13:52:54 +0200

    try

--- philo_one/srcs/alive.c
+++ philo_one/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/10/07 13:50:29 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/10/07 13:52:45 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,23 +48,24 @@ int		ft_lock_forks(t_phi *phi)
 	// if (pthread_mutex_lock(phi->params->available))
 	// 	return (ft_error(ERROR_LOCK_MUTEX));
 	// here
-	// while (phi->left_fork->id == phi->right_fork->id ||
-	// // phi->left_fork->status == 1 || phi->right_fork->status == 1 ||
-	// // pthread_mutex_lock(phi->params->available) != 0)
-	// pthread_mutex_lock(phi->params->available) != 0 ||
-	// phi->left_fork->status == 1 || phi->right_fork->status == 1)
-	// {
-	// 	if (pthread_mutex_unlock(phi->params->available))
-	// 		return (ft_error(ERROR_UNLOCK_MUTEX));
-	// 	if (ft_is_dead(phi) == 1 || phi->params->game == 0)
-	// 		return (1);
-	// 	usleep(10);
-	// }
+	dprintf(2, "phi%d waiting availability\n", phi->id);
+	while (phi->left_fork->id == phi->right_fork->id ||
+	// phi->left_fork->status == 1 || phi->right_fork->status == 1 ||
+	// pthread_mutex_lock(phi->params->available) != 0)
+	pthread_mutex_lock(phi->params->available) != 0 ||
+	phi->left_fork->status == 1 || phi->right_fork->status == 1)
+	{
+		if (pthread_mutex_unlock(phi->params->available))
+			return (ft_error(ERROR_UNLOCK_MUTEX));
+		if (ft_is_dead(phi) == 1 || phi->params->game == 0)
+			return (1);
+		usleep(10);
+	}
 
 	// here
-	dprintf(2, "phi%d waiting availability\n", phi->id);
-	if (pthread_mutex_lock(phi->params->available))
-		return (ft_error(ERROR_LOCK_MUTEX));
+	// dprintf(2, "phi%d waiting availability\n", phi->id);
+	// if (pthread_mutex_lock(phi->params->available))
+	// 	return (ft_error(ERROR_LOCK_MUTEX));
 	// here
 	dprintf(2, "phi%d waiting left-fork\n", phi->id);
 	if (pthread_mutex_lock(phi->left_fork->mutex))
