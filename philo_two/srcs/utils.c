e8baa935efe3154003e6f0baf9b5c3c6a714f25a
Clement Bussiere <you@example.com>
2020-10-09 09:42:50 +0200

    try

--- philo_two/srcs/utils.c
+++ philo_two/srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 15:43:27 by cbussier          #+#    #+#             */
-/*   Updated: 2020/10/06 21:49:16 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/10/09 09:42:05 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,12 +51,9 @@ int		ft_atoi(const char *str)
 
 int		ft_get_timestamp(struct timeval start, struct timeval t)
 {
-	int	start_ms;
-	int	t_ms;
 	int	timestamp;
 
-	start_ms = start.tv_sec * 1000 + start.tv_usec / 1000;
-	t_ms = t.tv_sec * 1000 + t.tv_usec / 1000;
-	timestamp = t_ms - start_ms;
+	timestamp = 1000 * (t.tv_sec - start.tv_sec) +
+	(t.tv_usec - start.tv_usec) * 0.001;
 	return (timestamp);
 }
