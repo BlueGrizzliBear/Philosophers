0cc86105a9ef4ef62ba4c6799149e3163857430a
Clement Bussiere <you@example.com>
2020-11-16 13:38:08 +0100

    modif:

--- philo_one/srcs/alive.c
+++ philo_one/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/11/16 13:36:40 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/11/16 13:37:59 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,23 +32,8 @@ int		ft_standby(t_phi *phi, int time)
 
 	while (ft_get_timestamp(standby_start, now) < time)
 	{
-		if (pthread_mutex_lock(phi->params->game_status))
-			return (ft_error(ERROR_LOCK_MUTEX));
-		if (!phi->params->game)
-		{
-			if (pthread_mutex_unlock(phi->params->game_status))
-				return (ft_error(ERROR_UNLOCK_MUTEX));
+		if (!phi->params->game || ft_is_dead(phi))
 			return (-1);
-		}
-		if (pthread_mutex_unlock(phi->params->game_status))
-			return (ft_error(ERROR_UNLOCK_MUTEX));
-		if (ft_is_dead(phi))
-			return (-1);
-
-		if (ft_is_dead(phi))
-			return (-1);
-		// if (!phi->params->game || ft_is_dead(phi))
-		// 	return (-1);
 		gettimeofday(&now, NULL);
 	}
 	return (0);
