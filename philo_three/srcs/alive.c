d6429fbff5229c5f1b0167c7d2d51492fcba60ef
Clement Bussiere <you@example.com>
2020-11-20 13:26:01 +0100

    modif

--- philo_three/srcs/alive.c
+++ philo_three/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/11/20 13:12:01 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/11/20 13:25:52 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,13 +81,12 @@ int		ft_eat_sleep_think(t_phi *phi)
 		return (ret < 0 ? ret : 1);
 	if ((ret = ft_display(phi, " is eating\n")))
 		return (ret < 0 ? -2 : ft_error(ERROR_DISPLAY));
-	phi->has_eaten++;
-	if (phi->params->nb_time_phi_must_eat != -1 &&
-	phi->has_eaten >= phi->params->nb_time_phi_must_eat)
-		return (-3);
 	gettimeofday(&phi->last_meal, NULL);
 	if ((ret = ft_standby(phi, phi->params->time_to_eat)) != 0)
 		return (ret < 0 ? -2 : ft_error(ERROR_STANDBY));
+	if (phi->params->nb_time_phi_must_eat != -1 &&
+	++phi->has_eaten >= phi->params->nb_time_phi_must_eat)
+		return (-3);
 	if (ft_unlock_forks(phi))
 		return (1);
 	if ((ret = ft_display(phi, " is sleeping\n")))
