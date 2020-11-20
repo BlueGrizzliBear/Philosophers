65a5b675186e945ed19390a0a1759dbd7f68fb62
Clement Bussiere <you@example.com>
2020-11-20 15:45:55 +0100

    m

--- philo_three/srcs/free.c
+++ philo_three/srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 15:43:27 by cbussier          #+#    #+#             */
-/*   Updated: 2020/11/16 15:59:29 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/11/20 15:45:50 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int		ft_free_philosophers(t_phi *phi, t_params *params)
 
 int		ft_free_semaphore(t_params *params)
 {
+	sem_unlock(params->game_status);
 	if (sem_close(params->game_status))
 		return (ft_error(ERROR_CLOSING));
 	if (sem_unlink("/game_status"))
