67c5d929d57a15d8cfdfdd113a1ad33d81577180
Clement Bussiere <you@example.com>
2020-11-21 16:03:01 +0100

    modif

--- philo_two/srcs/alive.c
+++ philo_two/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/11/21 10:01:05 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/11/21 16:01:35 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ int		ft_standby(t_phi *phi, int time)
 		return (-1);
 	while (ft_get_timestamp(standby_start, now) < time)
 	{
-		if (!phi->params->game || ft_is_dead(phi))
+		if (ft_is_over(phi) || ft_is_dead(phi))
 			return (-1);
 		gettimeofday(&now, NULL);
 	}
@@ -52,12 +52,14 @@ int		ft_lock_forks(t_phi *phi)
 	ret = 0;
 	while (phi->params->forks_nb < 2)
 	{
-		if (!phi->params->game || ft_is_dead(phi))
+		if (ft_is_over(phi) || ft_is_dead(phi))
 			return (-1);
 	}
 	if (sem_wait(phi->params->forks) || sem_wait(phi->params->forks))
 		return (ft_error(ERROR_LOCK_SEM));
 	phi->params->forks_nb -= 2;
+	if (ft_is_over(phi) || ft_is_dead(phi))
+		return (-2);
 	if ((ret = ft_display(phi, " has taken a fork\n")) ||
 	(ret = ft_display(phi, " has taken a fork\n")))
 		return (ret < 0 ? -2 : ft_error(ERROR_DISPLAY));
@@ -79,6 +81,8 @@ int		ft_eat_sleep_think(t_phi *phi)
 	ret = 0;
 	if ((ret = ft_lock_forks(phi)) != 0)
 		return (ret < 0 ? ret : 1);
+	if (ft_is_over(phi) || ft_is_dead(phi))
+		return (-2);
 	if ((ret = ft_display(phi, " is eating\n")))
 		return (ret < 0 ? -2 : ft_error(ERROR_DISPLAY));
 	gettimeofday(&phi->last_meal, NULL);
@@ -89,10 +93,14 @@ int		ft_eat_sleep_think(t_phi *phi)
 		return (-3);
 	if (ft_unlock_forks(phi))
 		return (1);
+	if (ft_is_over(phi) || ft_is_dead(phi))
+		return (-1);
 	if ((ret = ft_display(phi, " is sleeping\n")))
 		return (ret < 0 ? -1 : ft_error(ERROR_DISPLAY));
 	if ((ret = ft_standby(phi, phi->params->time_to_sleep)) != 0)
 		return (ret < 0 ? -1 : ft_error(ERROR_STANDBY));
+	if (ft_is_over(phi) || ft_is_dead(phi))
+		return (-1);
 	if ((ret = ft_display(phi, " is thinking\n")))
 		return (ret < 0 ? -1 : ft_error(ERROR_DISPLAY));
 	return (0);
