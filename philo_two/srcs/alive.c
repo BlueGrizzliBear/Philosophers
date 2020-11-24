72bbe89f0127b39f3ba53302a43816fe2b0b8c2e
Clement Bussiere <you@example.com>
2020-11-24 09:18:56 +0100

    m

--- philo_two/srcs/alive.c
+++ philo_two/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/11/24 09:12:37 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/11/24 09:18:55 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,13 +56,13 @@ int		ft_lock_forks(t_phi *phi)
 		if (ft_is_over(phi) || ft_is_dead(phi))
 			return (-1);
 	}
-	// if (sem_wait(phi->params->order))
-	// 	return (ft_error(ERROR_LOCK_SEM));
-	// if (sem_post(phi->params->order))
-	// 	return (ft_error(ERROR_UNLOCK_SEM));
-	if (sem_wait(phi->params->forks) || sem_wait(phi->params->forks))
+	if (sem_wait(phi->params->order))
 		return (ft_error(ERROR_LOCK_SEM));
 	order = (order + 1) % phi->params->nb;
+	if (sem_post(phi->params->order))
+		return (ft_error(ERROR_UNLOCK_SEM));
+	if (sem_wait(phi->params->forks) || sem_wait(phi->params->forks))
+		return (ft_error(ERROR_LOCK_SEM));
 	phi->params->forks_nb -= 2;
 	// if (ft_is_over(phi) || ft_is_dead(phi))
 	// 	return (-2);
