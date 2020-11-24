6c85f0a2497618e327dffd522c0b484a4b331daf
Clement Bussiere <you@example.com>
2020-11-24 09:09:36 +0100

    m

--- philo_two/srcs/alive.c
+++ philo_two/srcs/alive.c
@@ -6,7 +6,7 @@
 /*   By: cbussier <cbussier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/30 10:59:40 by cbussier          #+#    #+#             */
-/*   Updated: 2020/11/24 09:07:54 by cbussier         ###   ########lyon.fr   */
+/*   Updated: 2020/11/24 09:09:34 by cbussier         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,11 +58,11 @@ int		ft_lock_forks(t_phi *phi)
 	}
 	// if (sem_wait(phi->params->order))
 	// 	return (ft_error(ERROR_LOCK_SEM));
-	order = (order + 1) % phi->params->nb;
 	// if (sem_post(phi->params->order))
 	// 	return (ft_error(ERROR_UNLOCK_SEM));
 	if (sem_wait(phi->params->forks) || sem_wait(phi->params->forks))
 		return (ft_error(ERROR_LOCK_SEM));
+	order = (order + 1) % phi->params->nb;
 	phi->params->forks_nb -= 2;
 	if (ft_is_over(phi) || ft_is_dead(phi))
 		return (-2);
